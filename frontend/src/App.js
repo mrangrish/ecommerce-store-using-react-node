a83f3c9553fe01cad33592ddad11380184562de8
Gautam Sharma <37954581+mrangrish@users.noreply.github.com>
2024-06-08 01:35:14 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
+import Home from './user/Home';
+import Adminlogin from './admin/Adminlogin';
+import Dashboard from './admin/dashboard';
+import Addproduct from './admin/Product/Addproduct';
+import AllProduct from './admin/Product/AllProduct';
+import Toster from './admin/toster';
+import Signup from './user/Register/Signup';
+import Product from './user/Product/Product';
+import SingleProduct from './user/SingleProduct/SingleProduct';
+import Addtocart from './user/addtocart/addtocart';
+import ProductView from './admin/Product/ProductView';
+import AdminProductEdit from './admin/Product/AdminProductEdit';
+import AllCustomer from './admin/Customer/AllCustomer';
+import Shop from './user/ShopPage/Shop';
+import Login from './user/Login/Login';
+import OrderDetails from './user/orders/orderdetails';
+import Example from './example';
+function App() {
+    const [userId, setUserId] = useState(null);
+
+    useEffect(() => {
+        const fetchUserId = async () => {
+            try {
+                const response = await axios.get('http://localhost:8081/session', {
+                    withCredentials: true
+                });
+                setUserId(response.data.userId);
+
+            } catch (error) {
+                console.error('Error fetching session id:', error);
+            }
+        };
+        fetchUserId();
+    }, []);
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                {/* All Client area routes */}
+                <Route path='/signup' element={<Signup userId={userId} setUserId={setUserId} />} />
+                <Route path='/' element={<Home userId={userId} setUserId={setUserId} />} />
+                <Route path='/Login' element={<Login userId={userId} setUserId={setUserId} />} />
+                <Route path='/toseter' element={<Toster />} />
+                <Route path='/Product/:id' element={<Product userId={userId} setUserId={setUserId} />} />
+                <Route path='/SingleProduct/:id' element={<SingleProduct userId={userId} setUserId={setUserId} />} />
+                <Route path='/addtocart' element={<Addtocart userId={userId} setUserId={setUserId} />} />
+                <Route path='/shop' element={<Shop userId={userId} setUserId={setUserId} />} />
+                <Route path='/orders' element={<OrderDetails userId={userId} setUserId={setUserId} />} />
+                <Route path='/example' element={<Example />} />
+
+                {/* All Admin area routes */}
+                <Route path='/AdminProductView/:id' element={<ProductView />} />
+                <Route path='/AdminProductEdit/:id' element={<AdminProductEdit />} />
+                <Route path='/AllCustomer' element={<AllCustomer />} />
+                <Route path='/dash' element={<Dashboard />} />
+                <Route path='/admin-login' element={<Adminlogin />} />
+                <Route path='/addproduct' element={<Addproduct />} />
+                <Route path='/AllProduct' element={<AllProduct />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
---
+++ frontend/src/Test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Test()
+{
+    return(
+        <>
+        <h1>Test </h1>
+        </>
+    );
+}
+
+export default Test;
