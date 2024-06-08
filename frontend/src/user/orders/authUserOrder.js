610f8659e6d59d5cbc974808df711e4989113547
mamta <mk4029742@gmail.com>
2024-06-08 17:00:51 +0530

    Frontend Changes

--- frontend/src/user/orders/authUserOrder.js
+++ frontend/src/user/orders/authUserOrder.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import PhoneInput from "react-phone-number-input";
 import 'react-phone-number-input/style.css';
 import { ToastContainer, toast } from 'react-toastify';
+import './Orderdetails.css';
 import 'react-toastify/dist/ReactToastify.css';
 
 function AuthUserOrder({ userId, setUserId }) {
@@ -14,13 +15,14 @@ function AuthUserOrder({ userId, setUserId }) {
         zip_Code: ''
     });
 
-    const [checkuserId, setCheckUserId] = useState([]);
+    const [checkUserId, setCheckUserId] = useState([]);
     const [email, setEmail] = useState('');
     const [otp, setOtp] = useState('');
     const [otpSent, setOtpSent] = useState(false);
     const [otpVerified, setOtpVerified] = useState(false);
     const [registerMessage, setRegisterMessage] = useState('');
-    const [emailError, setemailError] = useState('');
+    const [emailError, setEmailError] = useState('');
+
     useEffect(() => {
         const fetchUserId = async () => {
             try {
@@ -82,28 +84,28 @@ function AuthUserOrder({ userId, setUserId }) {
     const handleInputChange = (name, value) => {
         setValues(prev => ({ ...prev, [name]: value }));
     };
-    const sendOtp = () => {
-    if (email.trim() === '') {
-       setemailError('Email Should be not empty');
-        return;
-    }
-    
-    axios.post('http://localhost:8081/mailverified/send-email-otp', { email })
-        .then(response => {
-            if (response.data.success) {
-                setOtpSent(true);
-                toast.success('OTP Sent Successfully. Please check your email!');
-            }
-        })
-        .catch(error => {
-            if (error.response && error.response.status === 400) {
-                setRegisterMessage('Email not found, please register first');
-            } else {
-                toast.error('Error sending OTP. Please try again.');
-            }
-        });
-};
 
+    const sendOtp = () => {
+        if (email.trim() === '') {
+            setEmailError('Email should not be empty');
+            return;
+        }
+
+        axios.post('http://localhost:8081/mailverified/send-email-otp', { email })
+            .then(response => {
+                if (response.data.success) {
+                    setOtpSent(true);
+                    toast.success('OTP Sent Successfully. Please check your email!');
+                }
+            })
+            .catch(error => {
+                if (error.response && error.response.status === 400) {
+                    setRegisterMessage('Email not found, please register first');
+                } else {
+                    toast.error('Error sending OTP. Please try again.');
+                }
+            });
+    };
 
     const verifyOtp = async () => {
         try {
@@ -135,117 +137,83 @@ function AuthUserOrder({ userId, setUserId }) {
                     draggable
                     pauseOnHover
                 />
-                {registerMessage ? (
-                    <div className="col-md-6">
-                        <div className="row">
-                            <div className="col-md-6 mb-3">
-                                <label htmlFor="exampleInputemail" className="form-label text-muted">Email*</label>
-                                <input type="email" className="form-control text-muted" value={email} />
-                            </div>
-                            <div className="col-md-6 mb-3">
-                                <label htmlFor="exampleInputName">Name*</label>
-                                <input type="text" className="form-control text-muted" />
-                            </div>
-                            <div className="col-md-6 mb-3 mt-3">
-                                <label htmlFor="exampleInputpassword">Password*</label>
-                                <input type="password" className="form-control text-muted" />
-                            </div>
-                            <div className="col-md-6 mb-3 mt-3">
-                                <label htmlFor="exampleInputaddress">Phone*</label>
-                                <PhoneInput value={values.phone} defaultCountry="IN" onChange={(value) => handleInputChange('phone', value)} ref={ref} placeholder="Enter Your Mobile number" />
-                            </div>
-                            <div className="col-md-6 mb-3 mt-3">
-                                <label htmlFor="exampleInputaddress">Address*</label>
-                                <input type="text" className="form-control text-muted"></input>
-                            </div>
-                            <div className="col-md-6 mb-3 mt-3">
-                                <label htmlFor="exampleInputCity" className="form-label text-muted">Town/City</label>
-                                <input type="text" className="form-control text-muted" />
-                            </div>
-                            <div className="col-md-6 mb-3 mt-3">
-                                <label htmlFor="exampleInputPostcode" className="form-label text-muted">Postcode / ZIP*</label>
-                                <input type="text" className="form-control text-muted" />
-                            </div>
-                        </div>
+                <div className="col-md-7">
+                    <div style={{ margin: "0 0", color: otpVerified ? "black" : "white", background: otpVerified ? "white" : "lightseagreen" }} className="shadow rounded p-3">
+                        <p style={{ fontSize: "large", fontWeight: "500", position: "relative", margin: "0" }}>
+                            {otpVerified ? `Logged in as: ${email}` : 'Login/Signup'}
+                        </p>
                     </div>
-                ) : (
-                    <div className="col-md-7">
-                        <div style={{ margin: "0 0", color: otpVerified ? "black" : "white", background: otpVerified ? "white" : "lightseagreen" }} className="shadow rounded p-3">
-                            <p style={{ fontSize: "large", fontWeight: "500", position: "relative", margin: "0" }}>
-                                Login/Signup {otpVerified && email}
-                            </p>
-                        </div>
-                        {checkuserId.length === 0 ? (
-                            <div>
-                                <div style={{ background: "lightgrey", padding: "3% 4%" }}>
-                                    <label htmlFor="exampleInputemail" className="form-label text-muted">Email*</label>
-                                    <input
-                                        type="email"
-                                        className="form-control text-muted"
-                                        value={email}
-                                        onChange={(e) => setEmail(e.target.value)}
-                                        disabled={otpSent}
-                                    />
-                                     {emailError && <span className='text-danger'>{emailError}</span>}
-                                     <div className='mt-2'>
+                    {checkUserId.length === 0 ? (
+                        <div>
+                            <div style={{ background: "lightgrey", padding: "3% 4%" }}>
+                                <label htmlFor="exampleInputemail" className="form-label text-muted">Email*</label>
+                                <input
+                                    type="email"
+                                    className="form-control text-muted input"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    disabled={otpSent}
+                                    placeholder="Enter Email"
+                                />
+                                {emailError && <span className='text-danger'>{emailError}</span>}
+                                <div className='mt-2'>
                                     {!otpSent && (
                                         <button onClick={sendOtp} className="btn btn-warning">Submit</button>
                                     )}
-                                    </div>
-                                    {otpSent && !otpVerified && (
-                                        <div className="mb-3">
-                                            <label htmlFor="exampleInputOtp" className="form-label text-muted">Enter OTP*</label>
-                                            <input
-                                                type="text"
-                                                className="form-control text-muted"
-                                                value={otp}
-                                                onChange={(e) => setOtp(e.target.value)}
-                                            />
-                                            <button onClick={verifyOtp} className="btn btn-success">Verify OTP</button>
-                                        </div>
-                                    )}
                                 </div>
+                                {otpSent && !otpVerified && (
+                                    <div className="mb-3">
+                                        <label htmlFor="exampleInputOtp" className="form-label text-muted">Enter OTP*</label>
+                                        <input
+                                            type="text"
+                                            className="form-control text-muted input"
+                                            value={otp}
+                                            onChange={(e) => setOtp(e.target.value)}
+                                        />
+                                        <button onClick={verifyOtp} className="btn btn-success">Verify OTP</button>
+                                    </div>
+                                )}
+                            </div>
+                            <div style={{ margin: "0 0", background: 'lightseagreen', color: "white" }} className="shadow rounded p-3">
+                                Address Details
+                            </div>
+                        </div>
+                    ) : (
+                        checkUserId.map((item, index) => (
+                            <div key={index}>
                                 <div style={{ margin: "0 0", background: 'lightseagreen', color: "white" }} className="shadow rounded p-3">
                                     Address Details
                                 </div>
-                            </div>
-                        ) : (
-                            checkuserId.map((item, index) => (
-                                <div key={index}>
-                                    <div style={{ margin: "0 0", background: 'lightseagreen', color: "white" }} className="shadow rounded p-3">
-                                        Address Details
+                                <div style={{ background: "lightgrey", padding: "3% 4%" }}>
+                                    <div className="row">
+                                        <div className="mb-3 mt-3 col-6">
+                                            <label htmlFor="exampleInputphone" className="form-label text-muted">Phone Number</label>
+                                            <PhoneInput value={values.phone} defaultCountry="IN" onChange={(value) => handleInputChange('phone', value)} ref={ref} placeholder="Enter Your Mobile number" />
+                                        </div>
+                                        <div className="mb-3 mt-3 col-6">
+                                            <label htmlFor="exampleInputAddress" className="form-label text-muted">Address*</label>
+                                            <input type="text" className="form-control input text-muted" value={values.Address} onChange={(e) => handleInputChange('Address', e.target.value)} />
+                                        </div>
                                     </div>
-                                    <div style={{ background: "lightgrey", padding: "3% 4%" }}>
-                                        <div className="row">
-                                            <div className="mb-3 mt-3 col-6">
-                                                <label htmlFor="exampleInputphone" className="form-label text-muted">Phone Number</label>
-                                                <PhoneInput value={values.phone} defaultCountry="IN" onChange={(value) => handleInputChange('phone', value)} ref={ref} placeholder="Enter Your Mobile number" />
-                                            </div>
-                                            <div className="mb-3 mt-3 col-6">
-                                                <label htmlFor="exampleInputAddress" className="form-label text-muted">Address*</label>
-                                                <input type="text" className="form-control text-muted" value={values.Address} onChange={(e) => handleInputChange('Address', e.target.value)} />
-                                            </div>
+                                    <div className="row">
+                                        <div className="mb-3 mt-3 col-6">
+                                            <label htmlFor="exampleInputCity" className="form-label text-muted">Town/City</label>
+                                            <input type="text" className="form-control input text-muted" value={values.City} onChange={(e) => handleInputChange('City', e.target.value)} />
                                         </div>
-                                        <div className="row">
-                                            <div className="mb-3 mt-3 col-6">
-                                                <label htmlFor="exampleInputCity" className="form-label text-muted">Town/City</label>
-                                                <input type="text" className="form-control text-muted" value={values.City} onChange={(e) => handleInputChange('City', e.target.value)} />
-                                            </div>
-                                            <div className="mb-3 mt-3 col-6">
-                                                <label htmlFor="exampleInputPostcode" className="form-label text-muted">Postcode / ZIP*</label>
-                                                <input type="text" className="form-control text-muted" value={values.zip_Code} onChange={(e)=> handleInputChange('zip_Code', e.target.value)} />
-                                            </div>
-                                        </div>  
-                                        <div className="form-group mt-3">
-                                            <button className="btn btn-danger" onClick={handleSubmit}>Change</button>
-                                            <button className="btn btn-primary">Next</button>
+                                        <div className="mb-3 mt-3 col-6">
+                                            <label htmlFor="exampleInputPostcode" className="form-label text-muted">Postcode / ZIP*</label>
+                                            <input type="text" className="form-control input text-muted" value={values.zip_Code} onChange={(e) => handleInputChange('zip_Code', e.target.value)} />
                                         </div>
+                                    </div>
+                                    <div className="form-group mt-3">
+                                        <button className="btn btn-danger" onClick={handleSubmit}>Change</button>
+                                        <button className="btn btn-primary">Next</button>
                                     </div>
                                 </div>
-                            ))
-                        )}
-                    </div>
-                )}
+                            </div>
+                        ))
+                    )}
+                </div>
                 <div className="col-5">
                     <div className="order-summary">
                         <h4>Order Summary</h4>
--- frontend/src/user/orders/orderdetails.js
+++ frontend/src/user/orders/orderdetails.js
@@ -4,14 +4,10 @@ import AuthUserOrder from "./authUserOrder";
 
 
 function OrderDetails({ userId, setUserId }) {
-
-
     return (
         <>
             <Header userId={userId} setUserId={setUserId} />
-
             <AuthUserOrder userId={userId} setUserId={setUserId} />
-
         </>
     )
 }
