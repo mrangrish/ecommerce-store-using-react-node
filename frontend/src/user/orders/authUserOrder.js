657d7c2e029248699280955061d6c2dd01d3a500
mamta <mk4029742@gmail.com>
2024-06-13 14:53:13 +0530

    Frontend Changes

--- frontend/src/user/orders/authUserOrder.js
+++ frontend/src/user/orders/authUserOrder.js
@@ -4,6 +4,7 @@ import PhoneInput from "react-phone-number-input";
 import 'react-phone-number-input/style.css';
 import { ToastContainer, toast } from 'react-toastify';
 import './Orderdetails.css';
+import OrderSummary from "./OrderSummary";
 import Validation from "./OrderinputValidation";
 import { IoCheckmark } from 'react-icons/io5';
 import 'react-toastify/dist/ReactToastify.css';
@@ -27,21 +28,10 @@ function AuthUserOrder({ userId, setUserId }) {
     const [otpVerified, setOtpVerified] = useState(false);
     const [registerMessage, setRegisterMessage] = useState('');
     const [emailError, setEmailError] = useState('');
-    const [addtocart, setAddtocart] = useState([]);
-    const [totalPrice, setTotalPrice] = useState(0);
     const [newuserOtp, setNewuserOtp] = useState(false);
     const [errors, setErrors] = useState({});
 
-    useEffect(() => {
-        const calculateTotalPrice = () => {
-            let totalPrice = 0;
-            addtocart.forEach(item => {
-                totalPrice += parseFloat(item.product_offerPrice || item.product_price) * parseInt(item.quantity || 1);
-            });
-            setTotalPrice(totalPrice);
-        };
-        calculateTotalPrice();
-    }, [addtocart]);
+
 
     useEffect(() => {
         const fetchUserId = async () => {
@@ -98,38 +88,6 @@ function AuthUserOrder({ userId, setUserId }) {
 
         fetchUserDetails();
     }, [otpVerified, userId]);
-
-    const getImageUrl = (jsonString) => {
-        try {
-            const images = JSON.parse(jsonString);
-            return `http://localhost:8081/images/${images[0]}`;
-        } catch (e) {
-            console.error('Error parsing image JSON:', e);
-            return '';
-        }
-    };
-
-    useEffect(() => {
-        const fetchAddtocartproduct = async () => {
-            try {
-                if (userId) {
-                    const response = await axios.get(`http://localhost:8081/routeaddtocart/Getaddtocart/${userId}`);
-                    setAddtocart(response.data);
-                    
-                } else {
-                    const cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
-                    setAddtocart(cartItems);
-                    
-                }
-            } catch (error) {
-                console.error('Error fetching cart items:', error);
-                const cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
-                setAddtocart(cartItems);
-                
-            }
-        };
-        fetchAddtocartproduct();
-    }, [userId]);
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -424,27 +382,7 @@ function AuthUserOrder({ userId, setUserId }) {
                     </div>
                 )}
                 <div className="col-5">
-                    <div className="shadow p-4 rounded bg-light">
-                        <h4>Cart Items</h4>
-                        <hr />
-                        {addtocart.map((item, index) => (
-                            <div key={index} className="d-flex justify-content-between align-items-center mb-3">
-                                <div>
-                                    <img src={getImageUrl(item.product_image)} alt={item.product_name} width="50" />
-                                    <span className="ms-3">{item.product_name}</span>
-                                </div>
-                                <div>
-                                    <span>{item.quantity} x ₹{item.product_offerPrice || item.product_price}</span>
-                                </div>
-                            </div>
-                        ))}
-                        <hr />
-                        <div className="d-flex justify-content-between align-items-center">
-                           <h5>Total Price</h5>
-                         <h5>₹{totalPrice.toFixed(2)}</h5>
-                        </div>
-                    </div>
-
+                <OrderSummary userId={userId} setUserId={setUserId} />
                 </div>
             </div>
         </div>
