75ac731503dab599a3659028eee8bc04a53c53de
mamta <mk4029742@gmail.com>
2024-06-17 13:03:46 +0530

    Frontend Changes

--- frontend/src/user/orders/authUserOrder.js
+++ frontend/src/user/orders/authUserOrder.js
@@ -1,7 +1,6 @@
-import React, { useEffect, useState, createRef } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import PhoneInput from "react-phone-number-input";
-import 'react-phone-number-input/style.css';
+
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './Orderdetails.css';
@@ -9,9 +8,11 @@ import OrderSummary from "./OrderSummary";
 import Validation from "./OrderinputValidation";
 import { IoCheckmark } from 'react-icons/io5';
 import $ from "jquery";
+import RegisterUser from "./RegsiterUser";
+import UserAddressForm from "./UserAddressForm";
 
 function AuthUserOrder({ userId, setUserId }) {
-    const ref = createRef();
+
     const [values, setValues] = useState({
         phone: '',
         Address: '',
@@ -21,6 +22,7 @@ function AuthUserOrder({ userId, setUserId }) {
         name: '',
         password_view: '',
     });
+    const [list, setList] = useState(false)
 
     const [checkUserId, setCheckUserId] = useState([]);
     const [email, setEmail] = useState('');
@@ -37,7 +39,7 @@ function AuthUserOrder({ userId, setUserId }) {
         const fetchUserId = async () => {
             try {
                 const userResponse = await axios.get(`http://localhost:8081/orderdetails/orderUserId/${userId}`);
-                
+
                 if (userResponse.status === 201) {
                     setAddress('User order details not existed.');
                     setValues({
@@ -66,20 +68,20 @@ function AuthUserOrder({ userId, setUserId }) {
                 console.error('Error fetching user details:', err);
             }
         };
-    
+
         if (userId) {
             fetchUserId();
         }
     }, [userId]);
-    
-      useEffect(() => {
+
+    useEffect(() => {
         if (checkUserId.length > 0) {
             $('#checkuserid').css('display', 'none');
         } else {
             $('#checkuserid').css('display', 'block');
         }
     }, [checkUserId.length]);
-    
+
     useEffect(() => {
         const fetchUserDetails = async () => {
             if (otpVerified) {
@@ -233,56 +235,29 @@ function AuthUserOrder({ userId, setUserId }) {
         }
     };
 
+
+    const handleChangeAddress = async (e) => {
+        e.preventDefault()
+        if (e.target.value) {
+            setList(!list)
+        }
+        setList(!list)
+    }
     return (
-        <div className="container mt-5">
+        <div className="container-fluid mt-5">
             <div className="row justify-content-center">
                 <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
                 {registerMessage ? (
-                    <div className="col-md-7">
-                        <div style={{ margin: "0 0", color: "white", background: "lightseagreen" }} className="shadow rounded p-3">
-                            <p style={{ fontSize: "large", fontWeight: "500", position: "relative", margin: "0" }}>Update Your Name and Password</p>
-                        </div>
-                        <div style={{ background: "lightgrey", padding: "3% 4%" }}>
-                            <div className="row">
-                                <div className="mt-3 col-6">
-                                    <label>Name</label>
-                                    <input
-                                        type="text"
-                                        name="name"
-                                        placeholder="Enter Your Name"
-                                        value={values.name}
-                                        className="form-control input"
-                                        onChange={(e) => handleInputChange('name', e.target.value)}
-                                    />
-                                    {errors.name && <span style={{ color: "red" }}>{errors.name}</span>}
-                                </div>
-                                <div className="mt-3 col-6">
-                                    <label>Password</label>
-                                    <input
-                                        type="text"
-                                        name="password_view"
-                                        placeholder="Enter Password"
-                                        value={values.password_view}
-                                        className="form-control input"
-                                        onChange={(e) => handleInputChange('password_view', e.target.value)}
-                                    />
-                                    {errors.password_view && <span style={{ color: "red" }}>{errors.password_view}</span>}
-                                </div>
-                            </div>
-                            <button className="btn btn-primary mt-3" onClick={handleUpdateDetails}>
-                                Update Details
-                            </button>
-                        </div>
-                    </div>
+                    <RegisterUser values={values} handleInputChange={handleInputChange} errors={errors} handleUpdateDetails={handleUpdateDetails} />
                 ) : (
-                    <div className="col-md-6">
+                    <div className="col-md-7">
                         {checkUserId.length === 0 ? (
                             <>
-                                <div id="checkuserid" style={{ margin: "0 0", color: "white", background: "lightseagreen"}} className="shadow rounded p-3">
+                                <div id="checkuserid" style={{ margin: "0 0", color: "white", background: "lightseagreen" }} className="shadow rounded p-3">
                                     <p style={{ fontSize: "large", fontWeight: "500", position: "relative", margin: "0" }}>Login/Signup</p>
                                 </div>
                                 {!otpVerified ? (
-                                    <div id="checkuserid" style={{ background: "lightgrey", padding: "3% 4%"}}>
+                                    <div id="checkuserid" style={{ background: "lightgrey", padding: "3% 4%" }}>
                                         <label htmlFor="exampleInputEmail" className="form-label text-muted">Email*</label>
                                         <input
                                             type="email"
@@ -327,68 +302,55 @@ function AuthUserOrder({ userId, setUserId }) {
                             </>
                         ) : (
                             <>
-                            <div style={{ margin: "0 0", color: "black", background: "white" }} className="shadow rounded p-3">
-                             <p style={{ fontSize: "large", fontWeight: "500", position: "relative", margin: "0" }}>
-                                <IoCheckmark style={{ color: "green", fontSize: "33px" }} /> {values.useremail}
-                             </p>
+                                <div style={{ margin: "0 0", color: "black", background: "white" }} className="shadow rounded p-3">
+                                    <p style={{ fontSize: "large", fontWeight: "500", position: "relative", margin: "0" }}>
+                                        <IoCheckmark style={{ color: "green", fontSize: "33px" }} /> {values.useremail}
+                                    </p>
 
                                 </div>
                                 {address ? (
                                     <>
-                                        <div style={{ margin: "0 0", color: "white", background: "lightseagreen" }} className="shadow rounded p-3">
-                                            <p style={{ fontSize: "large", fontWeight: "500", position: "relative", margin: "0" }}>Please Add Your Address Details</p>
-                                        </div>
-                                        <div style={{ background: "lightgrey", padding: "3% 4%" }}>
-                                            <form onSubmit={handleSubmit}>
-                                                <div className="row">
-                                                    <div className="mt-3 col-6">
-                                                        <label>Phone Number</label>
-                                                        <PhoneInput ref={ref} defaultCountry="IN" value={values.phone}  placeholder="Enter your phone" onChange={(value) => handleInputChange('phone', value)} />
-                                                        {errors.phone && <span style={{ color: "red" }}>{errors.phone}</span>}
-                                                    </div>
-                                                    <div className="mt-3 col-6">
-                                                        <label>Address</label>
-                                                        <input type="text" name="Address" className="form-control input" placeholder="Enter Your Address" value={values.Address} onChange={(e) => handleInputChange('Address', e.target.value)}/>
-                                                        {errors.Address && <span style={{ color: "red" }}>{errors.Address}</span>}
-                                                    </div>
-                                                    <div className="mt-3 col-6">
-                                                        <label>City</label>
-                                                        <input type="text" name="City" placeholder="Enter City" className="form-control input" value={values.City} onChange={(e) => handleInputChange('City', e.target.value)}/>
-                                                        {errors.City && <span style={{ color: "red" }}>{errors.City}</span>}
-                                                    </div>
-                                                    <div className="mt-3 col-6">
-                                                        <label>Zip Code</label>
-                                                        <input type="text" name="zip_Code" placeholder="Enter Zipcode" value={values.zip_Code} className="form-control input" onChange={(e) => handleInputChange('zip_Code', e.target.value)}/>
-                                                        {errors.zip_Code && <span style={{ color: "red" }}>{errors.zip_Code}</span>}
-                                                    </div>
-                                                </div>
-                                                <button className="btn btn-primary mt-3" type="submit">
-                                                    Add Address
-                                                </button>
-                                            </form>
-                                        </div>
+                                        <UserAddressForm
+                                            values={values}
+                                            errors={errors}
+                                            handleInputChange={handleInputChange}
+                                            handleSubmit={handleSubmit}
+                                        />
                                     </>
                                 ) : (
                                     <>
-                                        <div style={{ margin: "0 0", color: "black", background: "white" }} className="shadow rounded p-3">
-                                            <div className="row">
-                                                <div className="col-8">
-                                                    <p style={{ fontSize: "large", fontWeight: "500", position: "relative", margin: "0" }}>
-                                                        <IoCheckmark style={{ color: "green", fontSize: "33px" }} /> {values.Address}
-                                                    </p>
-                                                </div>
-                                                <div className="col-4">
-                                                    <button className="btn btn-primary">Change</button>
+                                        {!list ?
+                                            <div style={{ margin: "0 0", color: "black", background: "white" }} className="shadow rounded p-3">
+                                                <div className="row">
+                                                    <div className="col-8">
+                                                        <p style={{ fontSize: "large", fontWeight: "500", position: "relative", margin: "0" }}>
+                                                            <IoCheckmark style={{ color: "green", fontSize: "33px" }} /> {values.Address}
+                                                        </p>
+                                                    </div>
+                                                    <div className="col-4">
+                                                        <button className="btn btn-primary" onClick={handleChangeAddress}>Change</button>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
+                                            :
+                                            <>
+                                                <div style={{ margin: "0 0", color: "white", background: "lightseagreen" }} className="shadow rounded p-3">
+                                                    <p style={{ fontSize: "large", fontWeight: "500", position: "relative", margin: "0" }}>Delivery Address</p>
+                                                </div>
+
+                                                <div style={{ background: "lightgrey", padding: "3% 4%" }}>
+                                                    <p><b>{values.name}</b> {values.phone}</p>
+                                                    {values.Address}{values.City}{values.zip_Code}
+                                                </div>
+                                            </>
+                                        }
                                     </>
                                 )}
                             </>
                         )}
                     </div>
                 )}
-                <div className="col-5">
+                <div className="col-4">
                     <OrderSummary userId={userId} setUserId={setUserId} />
                 </div>
             </div>
