21d33d46218d33e0cb268380d2923a63ef7f515d
mamta <mk4029742@gmail.com>
2024-06-27 16:28:57 +0530

    Frontend Changes

--- frontend/src/user/Home.js
+++ frontend/src/user/Home.js
@@ -6,7 +6,7 @@ import Latest from './latest/latest';
 import Productcategories from './Categories/productcategories';
 import Footer from './footer/footer';
 import axios from 'axios';
-
+import CategoriesDropDown from './Categories-dropdown/categories-dropdown';
 function Home({ userId, setUserId }) {
     const [addtocartcount, setAddtocartcount] = useState(0);
     const [localStoragecount, setLocalStoragecount] = useState(0);
@@ -30,9 +30,10 @@ function Home({ userId, setUserId }) {
     return (
         <div>
             <Header userId={userId} setUserId={setUserId} addtocartcount={addtocartcount} localStoragecount={localStoragecount} />
+            <CategoriesDropDown />
             <Slideshow />
-            <Productcategories userId={userId} />
-            <Latest userId={userId} setUserId={setUserId} setAddtocartcount={setAddtocartcount} setLocalStoragecount={setLocalStoragecount}/>
+            {/* <Productcategories userId={userId} /> */}
+            <Latest userId={userId} setUserId={setUserId} setAddtocartcount={setAddtocartcount} setLocalStoragecount={setLocalStoragecount} />
             <Footer />
         </div>
     );
